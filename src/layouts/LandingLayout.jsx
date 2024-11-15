f97a5b3f2ef8184e81fbf2781751229da3df7361
remeessalam <remeesreme4u@gmail.com>
2024-11-15 16:10:29 +0530

    erin soft done

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -10,6 +10,8 @@ import SectionThree from "../app/landingPage/components/SectionThree";
 import SectionBlogs1 from "../app/sections/home/index/section-blogs1";
 import Sectionourresult from "../app/sections/home/index/section-our-result";
 import SectionFour from "../app/landingPage/components/SectionFour";
+import { portfolioDataApp, portfolioDataWeb } from "../globals/constants";
+import FormComponent from "../app/landingPage/components/FormComponent";
 
 function RootLandingLayout() {
   const { pathname } = useLocation();
@@ -25,6 +27,15 @@ function RootLandingLayout() {
       });
     }
   };
+
+  const modifiedPathname = pathname.replace("/", "");
+  console.log(modifiedPathname, "asdlfkajsd");
+
+  const portfolioData =
+    modifiedPathname === "web-development"
+      ? portfolioDataWeb
+      : portfolioDataApp;
+
   return (
     <>
       <div className="page-wraper">
@@ -40,18 +51,45 @@ function RootLandingLayout() {
 
         <div className="page-content">
           <div ref={homeRef}>
-            <Section page={pathname} />
+            <Section page={modifiedPathname} />
           </div>
+          <FormComponent />
           <SectionTwo
-            page={pathname}
+            page={modifiedPathname}
             ref={servicesRef}
             scrollToSection={scrollToSection}
           />
           <div ref={servicesRef}>
-            <SectionThree page={pathname} />
+            <SectionThree page={modifiedPathname} />
+          </div>
+          <div className="portfolio-container">
+            <div className="wrapper">
+              <div className="header-content">
+                <h2 className="portfolio-heading">Our Portfolio</h2>
+                <p className="portfolio-description">
+                  Explore some of the successful projects we have delivered,
+                  showcasing our expertise across diverse domains. Here are some
+                  of our valued clients and the work we've done for them.
+                </p>
+              </div>
+              <ul className="portfolio-grid">
+                {portfolioData.map((project, index) => (
+                  <li key={index} className="portfolio-item">
+                    <div className="project-image-wrapper">
+                      <img
+                        src={project.image}
+                        alt={`${project.name} app preview`}
+                        className="project-image"
+                      />
+                      <strong className="project-name">{project.name}</strong>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
           <div ref={aboutRef}>
-            <SectionBlogs1 />
+            <SectionBlogs1 page={modifiedPathname} />
           </div>
           <Sectionourresult />
           <div ref={contactRef}>
